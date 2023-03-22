364d4c962fe52d5d4b21585e2787002b861372ee
Luiz <luizmay2@gmail.com>
2023-03-22 14:38:49 -0300

    imc calculation

--- script.js
+++ script.js
@@ -25,3 +25,44 @@ function go(currentStep, nextStep) {
 
   displayBlock.style.display = "block"
 }
+
+function validate() {
+  const peso = document.getElementById("peso")
+  const altura = document.getElementById("altura")
+
+  peso.style.border = "none"
+  altura.style.border = "none"
+
+  if (!peso.value || !altura.value) {
+    if (!peso.value && !altura.value) {
+      peso.style.border = "1px solid red"
+      altura.style.border = "1px solid red"
+    } else if (!peso.value) {
+      peso.style.border = "1px solid red"
+    } else {
+      altura.style.border = "1px solid red"
+    }
+  } else {
+    let imc = peso.value / (altura.value * altura.value)
+    const result = document.getElementById("resultado")
+
+    if (imc < 18.5) {
+      result.innerHTML = "Magreza | Obesidade 0"
+      result.style.color = "yellow"
+    } else if (imc >= 18.5 && imc < 25) {
+      result.innerHTML = "Normal | Obesidade 0"
+      result.style.color = "#51FF22"
+    } else if (imc >= 25 && imc < 30) {
+      result.innerHTML = "Sobrepeso | Obesidade 1"
+      result.style.color = "yellow"
+    } else if (imc >= 30 && imc < 40) {
+      result.innerHTML = "Obesidade | Obesidade 2"
+      result.style.color = "#FF7000"
+    } else if (imc >= 40) {
+      result.innerHTML = "Obesidade Grave | Obesidade 3"
+      result.style.color = "#FF2222"
+    }
+
+    go(2, 3)
+  }
+}
